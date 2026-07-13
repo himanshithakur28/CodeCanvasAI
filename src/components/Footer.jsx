2fe9a02b2d28419f15ac2549dd9e3f11c4b077bc
himanshithakur28 <himanshithakur282007@gmail.com>
2026-07-13 20:44:38 +0530

    Polish footer and finalize UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 function Footer() {
   return (
     <footer className="text-center py-6 bg-slate-900 border-t border-slate-700 text-slate-400">
-      Built for Next Byte Hacks 
+    <h1> Transforming Ideas into Production-Ready Blueprints</h1>
+        Powered by AI • Designed for Developers
     </footer>
   );
 }
